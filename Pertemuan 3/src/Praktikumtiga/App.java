b71f50f881bbd5c3e339906be4e69dc3438b116b
Ririn Verdawati <verdawatiririn@gmail.com>
2025-11-01 00:26:34 +0700

    Add files via upload

---
+++ Pertemuan 3/src/Praktikumtiga/App.java
@@ -0,0 +1,8 @@
+package Praktikumtiga;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Mahasiswa mahasiswa1 = new Mahasiswa("Rin", "IT", "4524210090", 3.19);
+        mahasiswa1.tampilData();
+    }
+}
